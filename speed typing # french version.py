72624b0016866dbb022a9a39f2ee0f09231d5e76
poulet roti ND <nicolas.dmt@laposte.net>
2025-04-18 12:08:49 +0200

    speed typing # french version.py

--- speed typing # french version.py
+++ speed typing # french version.py
@@ -1,6 +1,7 @@
 "speed typing program" or "programme de course d'écriture sur le clavier" # 16.04.2025
 
 import time
+from random import shuffle
 from tkinter import *
 from threading import Thread
 
@@ -25,24 +26,51 @@ nowword = 0
 # créer l'interface
 root = Tk()
 root.title('prgramme de course d\'écriture pour le clavier')
+timer = Label(root,text=repr(rtime),font='KristenITC 30',fg='dark blue')
+timer.pack(anchor='nw')
 wlabel = Label(root,font=('Arial',16)) # label où les mots seront affichés
-wlabel.pack()
+wlabel.pack(side='top')
 entry = Entry(root,font=('Arial',20),justify='center') # entrée où on écrit les mots
 entry.pack()
 
 # créer les fonctions nécessaire
 
-def endgame():
-    pass # à faire
-
-def chronometer():# fonction pour suivre le temps demandé puis faire le jeu
-    time.sleep(rtime)
-    root.unbind_all()
+def endgame():# fonction pour dire que c'est la fin du temps et demander si on veux recommencer
+    root.unbind_all('<Return>')# désactiver les touches
+    # effacer tout ce qu'on a écrit
+    entry.delete(0,'end')
+    wlabel.configure(text='')
     
-    endgame()
+    # faire un menu de pour demander ce quel'on veux faire dans un menu à part
+    top = Toplevel(root)
+    top.title('options')
+    
+    explicationL = Label(top,text='Le temps s\'est écoulé')# label pour expliquer ce qui s'est passé
+    explicationL.pack(fill='x',expand=True)
+    
+    frame = Frame(top)# frame pour placer les boutons "recommencer" et "quitter"
+    frame.pack(fill='x',expand=True)
+
+    restart = Button(frame,text='recommencer',command=lambda:[top.destroy(), begin(),chronometer()])# bouton refaire le jeu
+    restart.grid(row=0,column=0,ipadx=5,ipady=5)
+    quit_button = Button(frame,text='quitter',command=root.destroy)# bouton quitter le jeu
+    quit_button.grid(row=0,column=1,ipadx=5,ipady=5)
+
+def chronometer():# fonction arrêter le jeu au temps demandé
+    for i in range(rtime,-1,-1):# faire le décompte de rtime jusqu'à 0 inclus
+        time.sleep(1)
+        timer.configure(text=i)# met à jour le chronomètre après 1s
+        root.update()# met à jour la fenêtre root
+    
+    endgame()# active la fonction pour la fin de jeu
 
 def begin():# réinitialiser le jeu
     global nowword
+    
+    # connecter les fonctions avec l'interface
+    root.bind('<Return>',click_enter)
+
+    # initialiser les variables
     nowword = 0
     wlabel.configure(text=words[0])
     entry.delete(0,'end')
@@ -54,26 +82,28 @@ def compare():# vérifier si ce qu'on a tapé correspond au mot demandé
         return False
 
 def next():# passer à la question suivante
-    # si on arrive à la fin de la liste, cela créerait une erreur "list out of range" alors il faut faire une liste de mots (variable word) suffisamment grande
+    # si on arrive à la fin de la liste (ce qui n'arrivera probablement pas), cela créerait une erreur "list out of range" alors il faut faire une liste de mots (variable word) suffisamment grande
     global nowword
     nowword += 1
     wlabel.configure(text=words[nowword])
     entry.delete(0,'end')
 
-def badanswer():
+def badanswer():# fonction pour montrer que c'est faut, puis effacer ce qu'on a écrit
     entry.configure(fg='red')
     root.update()
     root.after(1750,entry.delete(0,'end'))
     entry.configure(fg='black')
 
-def click_enter(event):
+def click_enter(event):# Quand on clique sur la touche 'Enter', cette fonction sera activée.
+    # compare ce qu'on a écrit avec le mot demandé:
     if compare():
-        next()
+        next()# passe au mot suivant
     else:
-        badanswer()
-# connecter les fonctions avec l'interface
-root.bind('<Return>',click_enter)
+        badanswer()# dit que c'est faux
 
 # lancement de l'interface
 begin()
+t = Thread(target=chronometer)
+t.start()
 root.mainloop()
+print('end\n')
